96410d9f21a32c80f63c9a0191455813763daa65
José Moreira <josemoreiravarzim@gmail.com>
2016-12-19 20:12:05 +0000

    Fix example and prepare for autodeploys

--- example/log-webhook-payments/index.js
+++ example/log-webhook-payments/index.js
@@ -1,15 +1,19 @@
-var IfThenPay = require( 'ifthenpay' )
+require( 'babel-polyfill' )
+
+var IfThenPay = require( 'ifthenpay' ).IfThenPay
 var http = require( 'http' )
 
-var SERVER = http.createServer().listen(80)
 
+var PORT = process.env.PORT || 8901
 var ENTITY = process.env.ENTITY || '99999'
 var SUB_ENTITY = process.env.SUB_ENTITY || '999'
 var PRE_SHARED_KEY = process.env.PRE_SHARED_KEY || '123456789012345678901234567890'
 
+var SERVER = http.createServer().listen(PORT)
+
 var ifthenpay = new IfThenPay({
   entity: ENTITY,
-  subentity: SUBENTITY,
+  subentity: SUB_ENTITY,
   webhook: {
     autosetup: true,
     server: SERVER,
